ae617fd4d8a07de49b3c455671a2cfacc7af1100
VladyBarvy <vladbarskoff@yandex.ru>
2025-01-04 20:57:49 +0300

    040125

--- src/index.js
+++ src/index.js
@@ -1,29 +1,142 @@
 import fastify from 'fastify';
 import view from '@fastify/view';
-import pug from 'pug';
+import pug from 'pug';  // https://pugjs.org/api/getting-started.html
+import sanitize from 'sanitize-html';
+import fastifyWebSocket from 'fastify-websocket';
 
 const app = fastify();
 const port = 3000;
 
-// Подключаем pug через плагин
-await app.register(view, { engine: { pug } });
 
+
+// Регистрируем плагин для работы с WebSocket
+app.register(fastifyWebSocket);
+
+
+
+// Обработчик для WebSocket
+app.get('/chat', { websocket: true }, (connection, request) => {
+  connection.on('message', message => {
+    console.log('Сообщение от клиента:', message);
+    connection.send(`Вы написали: ${message}`);
+  });
+});
+
+
+
+// Подключаем pug через плагин
+//await app.register(view, { engine: { pug } });
+
+
+// app.get('/users', (req, res) => {
+//   res.send('GET /users');
+// });
+
+// app.post('/users', (req, res) => {
+//   res.send('POST /users');
+// });
+
+
+
+
+// Обработчик GET-запроса по адресу /borsy
+app.get('/borsy', (req, res) => {
+  // Получаем параметр name из запроса
+  const name = req.query;
+
+  // Если name есть в запросе, приветствуем пользователя по имени, иначе - "Hello, World!"
+  //const greeting = name ? `Hello, ${name}!` : 'Hello, World!';
+  
+  // Отправляем приветствие как ответ
+  res.send(name);
+});
+
+
+
+// Обработчик GET-запроса по адресу /hello
+app.get('/hello', (req, res) => {
+  // Получаем параметр name из запроса
+  const name = req.query.name;
+
+  // Если name есть в запросе, приветствуем пользователя по имени, иначе - "Hello, World!"
+  const greeting = name ? `Hello, ${name}!` : 'Hello, World!';
+  
+  // Отправляем приветствие как ответ
+  res.send(greeting);
+});
+
+
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.type('html');
+//   res.send('<h1>Hello World!</h1>');
+// });
+
+
+
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user',
+    },
+  ],
+};
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = state.users.find((user) => user.id === parseInt(id));
+  if (!user) {
+    res.code(404).send({ message: 'User not found' });
+  } else {
+    res.send(user);
+  }
+});
+
+
+
+app.get('/courses/:courseId/lessons/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
+});
+
+
+//app.get('/', (req, res) => res.view('src/views/index'));
+
+
+// http://localhost:3000/users/%3Cscript%3Ealert('attack!')%3B%3C%2Fscript%3E
+// http://localhost:3000/users/&lt;script&gt;alert('attack!');&lt;/script&gt;
 /*
-app.get('/', (req, res) => {
+app.get('/users/:id', (req, res) => {
+  const escapedId = sanitize(req.params.id);
   res.type('html');
-  res.send('<h1>Hello World!</h1>');
+  res.send(`<h1>${escapedId}</h1>`);
 });
 */
 
-app.get('/', (req, res) => res.view('src/views/index'));
+
+
+// app.get('/', (req, res) => {
+//   res.view('src/views/index');
 
 
 
+//   const term = req.query.term;
+//    if (term !== null) {
+
+//    } else {
+
+//    }
+// console.log(term);
+//   const data = { term: state.courses };
+
+//   res.view('src/views/index', data);
 
 
 
-
-
+// });
 
 
 
@@ -66,3 +179,10 @@ app.get('/courses', (req, res) => {
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+
+
+
+
